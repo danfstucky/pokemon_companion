acf510ef4bfcb05c76d9ffeabb62270e71e3a967
Dan Stucky <danfstucky@gmail.com>
2019-10-15 20:56:46 -0500

    Setup basic pokedex

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon';
+
+export default {
+  components: {
+    Pokemon,
+  },
+};
+</script>
+
+<style lang="less">
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+  body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
