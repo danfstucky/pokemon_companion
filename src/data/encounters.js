b1201918165bb3260d66139a7ba6de0fb4077922
Dan Stucky <danfstucky@gmail.com>
2019-10-17 20:23:35 -0500

    Finish adding wild encounter data

--- src/data/encounters.js
+++ src/data/encounters.js
@@ -121,7 +121,7 @@ wildEncounterList = [
     ]
   },
   {
-    location: "Route 5 - Path to Luprar",  // Rename this route in game
+    location: "Route 7",
     encounters: [
       {
         terrain: "Land",
@@ -138,7 +138,7 @@ wildEncounterList = [
           {name: "PIKACHU"},
           {name: "ABRA"},
           {name: "MURKROW"},
-          {name: "STARLY"}
+          {name: "SPEAROW"}
         ],
       },
     ]
@@ -148,19 +148,488 @@ wildEncounterList = [
     encounters: [
       {
         terrain: "Land",
-        pokemon: [],
-      },
+        pokemon: [
+          {name: "EEVEE"},
+          {name: "MARILL"},
+          {name: "GROWLITHE"},
+          {name: "KOFFING"},
+          {name: "BELLOSSOM"},
+          {name: "STUNKY"},
+          {name: "LILLIPUP"},
+          {name: "JIGGLYPUFF"},
+          {name: "BUNEARY"},
+          {name: "GLAMEOW"},
+          {name: "GRIMER"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Echol Forest",
+    encounters: [
       {
-        terrain: "Cave",
-        pokemon: [],
+        terrain: "Land",
+        pokemon: [
+          {name: "BELLSPROUT"},
+          {name: "PINECO"},
+          {name: "RALTS"},
+          {name: "SWADLOON"},
+          {name: "BLITZLE"},
+          {name: "PURRLOIN"},
+          {name: "FARFETCHD"},
+          {name: "GOTHITA"},
+          {name: "SCYTHER"},
+          {name: "PINSIR"},
+          {name: "ODDISH"},
+          {name: "HOUNDOUR"},
+          {name: "SNEASEL"},
+          {name: "GLOOM"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Sideleaf City",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "KRABBY"},
+          {name: "SKORUPI"},
+          {name: "VENONAT"},
+          {name: "VULPIX"},
+          {name: "MARILL"},
+          {name: "BALTOY"},
+          {name: "SKARMORY"}
+        ],
       },
       {
         terrain: "Water",
-        pokemon: [],
+        pokemon: [
+          {name: "BASCULIN"},
+          {name: "DUCKLETT"},
+          {name: "FINNEON"}
+        ],
       },
       {
         terrain: "Fishing",
-        pokemon: [],
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "POLIWAG"},
+          {name: "SLOWPOKE"},
+          {name: "HORSEA"},
+          {name: "POLITOED"},
+          {name: "STARYU"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Secluded Glave",
+    encounters: [
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "TENTACOOL"},
+          {name: "CORSOLA"},
+          {name: "SQUIRTLE"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "POLIWAG"},
+          {name: "MANTINE"},
+          {name: "HORSEA"},
+          {name: "DRATINI"},
+          {name: "STARYU"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 12",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "SMEARGLE"},
+          {name: "SLOWPOKE"},
+          {name: "TOTODILE"},
+          {name: "KRABBY"},
+          {name: "SHUCKLE"},
+          {name: "MARILL"},
+          {name: "ELECTRIKE"},
+          {name: "WOOPER"},
+          {name: "SLOWBRO"},
+          {name: "DODUO"},
+          {name: "GRIMER"},
+          {name: "LICKITUNG"}
+        ],
+      },
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "CHINCHOU"},
+          {name: "WOOPER"},
+          {name: "TENTACOOL"},
+          {name: "SQUIRTLE"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "REMORAID"},
+          {name: "SLOWPOKE"},
+          {name: "OCTILLERY"},
+          {name: "POLITOED"},
+          {name: "MANTINE"},
+          {name: "DRATINI"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Koala Bay",
+    encounters: [
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "TENTACOOL"},
+          {name: "PSYDUCK"},
+          {name: "PSYDUCK"},
+          {name: "TENTACRUEL"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "SHELLDER"},
+          {name: "KRABBY"},
+          {name: "OCTILLERY"},
+          {name: "POLITOED"},
+          {name: "MANTINE"},
+          {name: "DRATINI"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 13",
+    encounters: [
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "WINGULL"},
+          {name: "WAILMER"},
+          {name: "SHARPEDO"},
+          {name: "DRATINI"},
+          {name: "SQUIRTLE"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "SHELLDER"},
+          {name: "KRABBY"},
+          {name: "QWILFISH"},
+          {name: "POLITOED"},
+          {name: "MANTINE"},
+          {name: "DRATINI"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Eagle Island",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "DITTO"},
+          {name: "MANKEY"},
+          {name: "METAPOD"},
+          {name: "KAKUNA"},
+          {name: "ABRA"},
+          {name: "MACHOP"},
+          {name: "SCYTHER"},
+          {name: "SUDOWOODO"},
+          {name: "PINECO"}
+        ],
+      },
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "SEEL"},
+          {name: "SHELLDER"},
+          {name: "POLIWAG"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "SHELLDER"},
+          {name: "KRABBY"},
+          {name: "OCTILLERY"},
+          {name: "MANTINE"},
+          {name: "DRATINI"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 14",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "JIGGLYPUFF"},
+          {name: "RALTS"},
+          {name: "TOGEPI"},
+          {name: "PIDGEY"},
+          {name: "PIDGEOTTO"},
+          {name: "HONCHKROW"},
+          {name: "CLEFAIRY"},
+          {name: "GLIGAR"}
+        ],
+      },
+      {
+        terrain: "Cave",
+        pokemon: [
+          {name: "RHYHORN"},
+          {name: "CUBONE"},
+          {name: "VOLTORB"},
+          {name: "STEELIX"},
+          {name: "ARON"},
+          {name: "BALTOY"},
+          {name: "ONIX"},
+          {name: "CLEFAIRY"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Haunted Fields",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "GASTLY"},
+          {name: "MISDREAVUS"},
+          {name: "LITWICK"},
+          {name: "BANETTE"},
+          {name: "DRIFLOON"},
+          {name: "DUSKULL"},
+          {name: "SABLEYE"},
+          {name: "FROSLASS"},
+          {name: "CUBONE"},
+          {name: "HAUNTER"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 15",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "PONYTA"},
+          {name: "DEINO"},
+          {name: "MAGNEMITE"},
+          {name: "KAKUNA"},
+          {name: "BEEDRILL"},
+          {name: "BUTTERFREE"},
+          {name: "SCYTHER"},
+          {name: "PINSIR"},
+          {name: "KANGASKHAN"},
+          {name: "QUAGSIRE"},
+          {name: "SUNKERN"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Harthril Lake",
+    encounters: [
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "GOLDEEN"},
+          {name: "POLIWAG"},
+          {name: "DRATINI"},
+          {name: "GYARADOS"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "TOTODILE"},
+          {name: "SLOWPOKE"},
+          {name: "DRATINI"},
+          {name: "GYARADOS"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 16",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "NIDORINO"},
+          {name: "PHANPY"},
+          {name: "HERACROSS"},
+          {name: "METAPOD"},
+          {name: "MILTANK"},
+          {name: "CHARMANDER"},
+          {name: "VOLTORB"},
+          {name: "DROWZEE"},
+          {name: "MEOWTH"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 17",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "CHIKORITA"},
+          {name: "AIPOM"},
+          {name: "CYNDAQUIL"},
+          {name: "SKARMORY"},
+          {name: "QUILAVA"},
+          {name: "MRMIME"},
+          {name: "TAUROS"},
+          {name: "ELECTABUZZ"},
+          {name: "DITTO"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Route 18",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "KOFFING"},
+          {name: "STANTLER"},
+          {name: "MEDITITE"},
+          {name: "WOBBUFFET"},
+          {name: "HOUNDOUR"},
+          {name: "SNUBBULL"},
+          {name: "CHANSEY"},
+          {name: "NIDORINA"},
+          {name: "SCYTHER"},
+          {name: "PINSIR"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "The Iciest Cave",
+    encounters: [
+      {
+        terrain: "Cave",
+        pokemon: [
+          {name: "SHELLDER"},
+          {name: "SEEL"},
+          {name: "SWINUB"},
+          {name: "CLOYSTER"},
+          {name: "DEWGONG"},
+          {name: "PILOSWINE"},
+          {name: "JYNX"},
+          {name: "CUBCHOO"},
+          {name: "HITMONCHAN"},
+          {name: "HITMONLEE"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Taelia Town",
+    encounters: [
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "POLIWHIRL"},
+          {name: "PSYDUCK"},
+          {name: "WARTORTLE"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "TENTACOOL"},
+          {name: "TENTACRUEL"},
+          {name: "FEEBAS"},
+          {name: "GYARADOS"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Salmon Plateau",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "NIDORINO"},
+          {name: "NIDORINA"},
+          {name: "NUMEL"},
+          {name: "DELIBIRD"},
+          {name: "NATU"},
+          {name: "CLEFABLE"},
+          {name: "SLAKOTH"},
+          {name: "MAGMAR"},
+          {name: "ELECTABUZZ"},
+          {name: "CHANSEY"}
+        ],
+      },
+      {
+        terrain: "Water",
+        pokemon: [
+          {name: "FEEBAS"},
+          {name: "POLIWHIRL"},
+          {name: "GOLDUCK"},
+          {name: "CROCONAW"},
+          {name: "WAILORD"}
+        ],
+      },
+      {
+        terrain: "Fishing",
+        pokemon: [
+          {name: "MAGIKARP"},
+          {name: "TENTACOOL"},
+          {name: "TENTACRUEL"},
+          {name: "KABUTO"},
+          {name: "OMANYTE"}
+        ],
+      }
+    ]
+  },
+  {
+    location: "Pokedex Island",
+    encounters: [
+      {
+        terrain: "Land",
+        pokemon: [
+          {name: "MOLTRES"},
+          {name: "MEWTWO"},
+          {name: "MEW"}
+        ],
       }
     ]
   },
