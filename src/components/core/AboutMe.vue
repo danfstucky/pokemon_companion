ef5ad71fd5d317dd5029d2463fcb80b22a4dcc1f
Daniel Stucky <danielstucky@daniels-mbp.lan>
2024-01-07 15:46:15 -0600

    Upgrade to Bootstrap v5

--- src/components/core/AboutMe.vue
+++ src/components/core/AboutMe.vue
@@ -2,36 +2,32 @@
   <div class="flex-wrapper">
     <div class="column column-2">
       <div class="card about-me-card">
-        <h4 class="card-header">
-          About Me
-        </h4>
+        <h4 class="card-header">About Me</h4>
         <div class="card-body">
-          <img src="./../../assets/images/dan_stucky.png">
+          <img src="./../../assets/images/dan_stucky.png" />
           <p>
-            I have been working as a Full-Stack Web Dev for 6 years and am currently employed by a smaller software
-            company named Storable. Our stack primarily consists of Rails with Postgres, but I get to play with
-            JS/React pretty often and recently even dabbled with 
-            <a href="https://vuejs.org/"
-               target="_blank"
-            >Vue.js</a>
-            and <a href="https://www.rust-lang.org/"
-                   target="_blank"
-            >Rust</a>. I love writing software and working
-            on projects that have a positive and meaningful influence on people's lives. Prior to software development,
-            I obtained a M.S. in molecular genetics and I'd like to someday combine my knowledge in
-            these two fields, but have not yet found the right opportunity.
+            I have been working as a Full-Stack Web Dev for 6 years and am
+            currently employed by a smaller software company named Storable. Our
+            stack primarily consists of Rails with Postgres, but I get to play
+            with JS/React pretty often and recently even dabbled with
+            <a href="https://vuejs.org/" target="_blank">Vue.js</a>
+            and <a href="https://www.rust-lang.org/" target="_blank">Rust</a>. I
+            love writing software and working on projects that have a positive
+            and meaningful influence on people's lives. Prior to software
+            development, I obtained a M.S. in molecular genetics and I'd like to
+            someday combine my knowledge in these two fields, but have not yet
+            found the right opportunity.
           </p>
           <p>
-            I built this site to provide some info on myself and keep a record of a few fun
-            projects I have engaged in outside of my employment. Take a look around and feel free to reach out.
+            I built this site to provide some info on myself and keep a record
+            of a few fun projects I have engaged in outside of my employment.
+            Take a look around and feel free to reach out.
           </p>
         </div>
       </div>
 
       <div class="card education-card">
-        <h4 class="card-header">
-          Education
-        </h4>
+        <h4 class="card-header">Education</h4>
         <div class="card-body">
           <ul class="list-unstyled">
             <li><i class="fas fa-graduation-cap" /> B.S. Computer Science</li>
@@ -41,15 +37,11 @@
           </ul>
           <ul class="list-unstyled">
             <li><i class="fas fa-graduation-cap" /> M.S. Molecular Genetics</li>
-            <li class="text-secondary">
-              Kansas State University (2013)
-            </li>
+            <li class="text-secondary">Kansas State University (2013)</li>
           </ul>
           <ul class="list-unstyled">
             <li><i class="fas fa-graduation-cap" /> B.S. Biology</li>
-            <li class="text-secondary">
-              Truman State University (2011)
-            </li>
+            <li class="text-secondary">Truman State University (2011)</li>
           </ul>
         </div>
       </div>
@@ -57,25 +49,21 @@
 
     <div class="column column-2">
       <div class="card skills-card">
-        <h4 class="card-header">
-          Skills
-        </h4>
+        <h4 class="card-header">Skills</h4>
         <div class="card-body">
           <HorizontalBarChart
             :data="skillData"
             :chart-options="{
               chartId: 'skills-graph-container',
               chartWidth: 500,
-              style: {barColor: '#3c8373', barHoverColor: '#63ac99'}
+              style: { barColor: '#3c8373', barHoverColor: '#63ac99' },
             }"
           />
         </div>
       </div>
 
       <div class="card contact-info-card">
-        <h4 class="card-header">
-          Contact
-        </h4>
+        <h4 class="card-header">Contact</h4>
         <div class="card-body">
           <p><i class="fas fa-map-marker-alt" /> Kansas City, MO</p>
           <p>
@@ -83,13 +71,15 @@
             <a
               href="https://www.linkedin.com/in/daniel-stucky-580464a8/"
               target="_blank"
-            > LinkedIn</a>
+            >
+              LinkedIn</a
+            >
           </p>
           <p>
-            <i class="fab fa-github" /> <a
-              href="https://github.com/danfstucky"
-              target="_blank"
-            >My GitHub</a>
+            <i class="fab fa-github" />
+            <a href="https://github.com/danfstucky" target="_blank"
+              >My GitHub</a
+            >
           </p>
         </div>
       </div>
@@ -98,7 +88,7 @@
 </template>
 
 <script>
-import HorizontalBarChart from './../shared/HorizontalBarChart';
+import HorizontalBarChart from "./../shared/HorizontalBarChart";
 
 export default {
   components: {
@@ -107,15 +97,15 @@ export default {
   data() {
     return {
       skillData: [
-        {text: 'Ruby/Rails', score: 10},
-        {text: 'JavaScript', score: 9},
-        {text: 'React', score: 7},
-        {text: 'Vue.js', score: 6},
-        {text: 'Java', score: 4},
-        {text: 'Python', score: 6},
-        {text: 'Postgres/SQL', score: 8},
-        {text: 'HTML/CSS', score: 10},
-        {text: 'git', score: 9}
+        { text: "Ruby/Rails", score: 10 },
+        { text: "JavaScript", score: 9 },
+        { text: "React", score: 9 },
+        { text: "Vue.js", score: 5 },
+        { text: "Java", score: 4 },
+        { text: "Python", score: 6 },
+        { text: "Postgres/SQL", score: 8 },
+        { text: "HTML/CSS", score: 10 },
+        { text: "git", score: 9 },
       ],
     };
   },
@@ -123,24 +113,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .flex-wrapper {
-    display: flex;
-    .column {
-      padding: 0px 10px;
-    }
+.flex-wrapper {
+  display: flex;
+  .column {
+    padding: 0px 10px;
+  }
 
-    .card {
-      margin: 10px;
-    }
+  .card {
+    margin: 10px;
+  }
 
-    .column-2 {
-      flex: 2;
-    }
-    .about-me-card {
-      img {
-        float: left;
-        padding-right: 10px;
-      }
+  .column-2 {
+    flex: 2;
+  }
+  .about-me-card {
+    img {
+      float: left;
+      padding-right: 10px;
     }
   }
+}
 </style>
--- src/components/core/DevProjects.vue
+++ src/components/core/DevProjects.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="projects-container">
     <div class="card">
-      <h4 class="card-header">
-        Pokemon Fan Game
-      </h4>
+      <h4 class="card-header">Pokemon Fan Game</h4>
       <div class="card-body flex-wrapper">
         <div class="column column-2">
           <img
             src="./../../assets/images/dev_projects/pokemon/salmon_red_logo.png"
             class="pokelogo"
-          >
+          />
           <p>
-            Pokemon fan game created inside RPG Maker using Ruby scripts to program game mechanics and
-            environment/character interaction. I completed this game in 2019 after 5 years of work, but it will
-            never be used for profit as it borrows many assets from the Nintendo franchise. I have always enjoyed
-            playing the Pokemon games and had a blast creating and play-testing this game with my own ideas.
-            It features 410 Pokemon, 8 gyms, an Elite 4 end-game battle, dozens of cities and routes to explore,
-            weather and night/day effects, egg hatching and daycare, and so much more!
+            Pokemon fan game created inside RPG Maker using Ruby scripts to
+            program game mechanics and environment/character interaction. I
+            completed this game in 2019 after 5 years of work, but it will never
+            be used for profit as it borrows many assets from the Nintendo
+            franchise. I have always enjoyed playing the Pokemon games and had a
+            blast creating and play-testing this game with my own ideas. It
+            features 410 Pokemon, 8 gyms, an Elite 4 end-game battle, dozens of
+            cities and routes to explore, weather and night/day effects, egg
+            hatching and daycare, and so much more!
           </p>
         </div>
 
@@ -24,124 +25,94 @@
           <div
             id="pokemon-carousel"
             class="carousel slide"
-            data-ride="carousel"
+            data-bs-ride="carousel"
           >
             <ol class="carousel-indicators">
               <li
-                data-target="#pokemon-carousel"
-                data-slide-to="0"
+                data-bs-target="#pokemon-carousel"
+                data-bs-slide-to="0"
                 class="active"
               />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="1"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="2"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="3"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="4"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="5"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="6"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="7"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="8"
-              />
-              <li
-                data-target="#pokemon-carousel"
-                data-slide-to="9"
-              />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="1" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="2" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="3" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="4" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="5" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="6" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="7" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="8" />
+              <li data-bs-target="#pokemon-carousel" data-bs-slide-to="9" />
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot1.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot2.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot3.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot4.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot5.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot6.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot7.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot8.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot9.png"
-                >
+                />
               </div>
               <div class="carousel-item">
                 <img
                   class="d-block w-100"
                   src="./../../assets/images/dev_projects/pokemon/screenshot10.png"
-                >
+                />
               </div>
             </div>
             <a
               class="carousel-control-prev"
               href="#pokemon-carousel"
               role="button"
-              data-slide="prev"
+              data-bs-slide="prev"
             >
               <div class="carousel-control-wrapper">
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                />
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
                 <span class="sr-only">Previous</span>
               </div>
             </a>
@@ -149,13 +120,10 @@
               class="carousel-control-next"
               href="#pokemon-carousel"
               role="button"
-              data-slide="next"
+              data-bs-slide="next"
             >
               <div class="carousel-control-wrapper">
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                />
+                <span class="carousel-control-next-icon" aria-hidden="true" />
                 <span class="sr-only">Next</span>
               </div>
             </a>
@@ -165,49 +133,63 @@
     </div>
 
     <div class="card">
-      <h4 class="card-header">
-        Tactical Arbitrage Command Line App
-      </h4>
+      <h4 class="card-header">Tactical Arbitrage Command Line App</h4>
       <div class="card-body">
         <p>
-          A Node.js command line application for finding great opportunities to buy and sell items from Walmart
-          and Amazon at a profit. It pulls down information for millions of items from both sites and outputs a
-          list of the top picks based on profit margin, buyer reviews, and consumer demand.
+          A Node.js command line application for finding great opportunities to
+          buy and sell items from Walmart and Amazon at a profit. It pulls down
+          information for millions of items from both sites and outputs a list
+          of the top picks based on profit margin, buyer reviews, and consumer
+          demand.
         </p>
         <i class="fab fa-github" />
         <a
           href="https://github.com/sulaimonlasisi/clearance-app"
           target="_blank"
-        > Project Github Link</a>
+        >
+          Project Github Link</a
+        >
       </div>
     </div>
 
     <div class="card">
-      <h4 class="card-header">
-        Smite Spec
-      </h4>
+      <h4 class="card-header">Smite Spec</h4>
       <div class="card-body flex-wrapper">
         <div class="column column-1">
           <p>
-            Smite Spec is intended for all players of the popular multiplayer online battle arena game, Smite.
-            It allows users to find player and clan statistics to help improve their play style and team performance
-            in matches or simply have fun viewing your friends' data. Unlike other third party Smite apps such as
-            SmiteRepo, Smite Spec focuses on the player. The user can easily track personal statistics for their
-            favorite and best god characters to play as, and compare their stats to in-game friends. It is only
-            available for Android.
+            Smite Spec is intended for all players of the popular multiplayer
+            online battle arena game, Smite. It allows users to find player and
+            clan statistics to help improve their play style and team
+            performance in matches or simply have fun viewing your friends'
+            data. Unlike other third party Smite apps such as SmiteRepo, Smite
+            Spec focuses on the player. The user can easily track personal
+            statistics for their favorite and best god characters to play as,
+            and compare their stats to in-game friends. It is only available for
+            Android.
           </p>
-          <i class="fab fa-github" /> 
-          <a
-            href="https://github.com/danfstucky/SmiteSpec"
-            target="_blank"
-          > Project Github Link</a>
+          <i class="fab fa-github" />
+          <a href="https://github.com/danfstucky/SmiteSpec" target="_blank">
+            Project Github Link</a
+          >
         </div>
         <div class="smite-spec-images-container column column-3">
           <table class="smite-spec-images">
             <tr>
-              <td><img src="./../../assets/images/dev_projects/smite_spec/screenshot1.png"></td>
-              <td><img src="./../../assets/images/dev_projects/smite_spec/screenshot2.png"></td>
-              <td><img src="./../../assets/images/dev_projects/smite_spec/screenshot3.png"></td>
+              <td>
+                <img
+                  src="./../../assets/images/dev_projects/smite_spec/screenshot1.png"
+                />
+              </td>
+              <td>
+                <img
+                  src="./../../assets/images/dev_projects/smite_spec/screenshot2.png"
+                />
+              </td>
+              <td>
+                <img
+                  src="./../../assets/images/dev_projects/smite_spec/screenshot3.png"
+                />
+              </td>
             </tr>
           </table>
         </div>
@@ -221,57 +203,64 @@
 </script>
 
 <style lang="scss" scoped>
-  .projects-container {
+.projects-container {
+  margin: 10px;
+
+  .card {
     margin: 10px;
-
-    .card {
-      margin: 10px;
+  }
+  .flex-wrapper {
+    display: flex;
+    .column {
+      padding: 0px 10px;
     }
-    .flex-wrapper {
-      display: flex;
-      .column {
-        padding: 0px 10px;
-      }
-      .column-1 { flex: 1; }
-      .column-2 { flex: 2; }
-      .column-3 { flex: 3; }
+    .column-1 {
+      flex: 1;
     }
-
-    #pokemon-carousel {
-      width: 600px;
-      .carousel-control-wrapper {
-        background: rgba(0, 0, 0, 0.445);
-        padding: 8px;
-        border-radius: 50%;
-        vertical-align: middle;
-        .carousel-control-next-icon, .carousel-control-prev-icon {
-          vertical-align: text-top;
-        }
-      }
-      .carousel-indicators {
-        background-color: rgba(0, 0, 0, 0.445);
-        width: 100%;
-        height: 50px;
-        margin: 0px;
-        align-items: center;
-
-        li {
-          width: 40px;
-          height: 5px;
-          margin-left: 10px
-        }
-      }
+    .column-2 {
+      flex: 2;
     }
-    .smite-spec-images-container {
+    .column-3 {
       flex: 3;
-      overflow: auto;
+    }
+  }
 
-      table.smite-spec-images {
-        img {
-          height: 70%;
-          padding: 10px;
-        }
+  #pokemon-carousel {
+    width: 600px;
+    .carousel-control-wrapper {
+      background: rgba(0, 0, 0, 0.445);
+      padding: 8px;
+      border-radius: 50%;
+      vertical-align: middle;
+      .carousel-control-next-icon,
+      .carousel-control-prev-icon {
+        vertical-align: text-top;
+      }
+    }
+    .carousel-indicators {
+      background-color: rgba(0, 0, 0, 0.445);
+      width: 100%;
+      height: 50px;
+      margin: 0px;
+      align-items: center;
+
+      li {
+        width: 40px;
+        height: 5px;
+        margin-left: 10px;
       }
     }
   }
+  .smite-spec-images-container {
+    flex: 3;
+    overflow: auto;
+
+    table.smite-spec-images {
+      img {
+        height: 70%;
+        padding: 10px;
+      }
+    }
+  }
+}
 </style>
